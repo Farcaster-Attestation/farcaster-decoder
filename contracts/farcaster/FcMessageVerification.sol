53a5796b2f43a902d95f7c4ec81ac480e3661b6e
Chomtana <Chomtana001@gmail.com>
2024-12-30 22:36:37 +0000

    MIT license

--- contracts/farcaster/FcMessageVerification.sol
+++ contracts/farcaster/FcMessageVerification.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 import {MessageHashUtils} from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
